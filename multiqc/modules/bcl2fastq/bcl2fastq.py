fda0bdb57eb552c1214fd43b401f5ca90da473cc
Tim Booth <tim.booth@ed.ac.uk>
2017-11-21 17:34:58 +0000

    Comment fix

--- multiqc/modules/bcl2fastq/bcl2fastq.py
+++ multiqc/modules/bcl2fastq/bcl2fastq.py
@@ -13,7 +13,7 @@ log = logging.getLogger(__name__)
 def rat(n, d, nan=None, mul=1.0):
     """ Calculate a ratio while avoiding division by zero errors.
         Strictly speaking we should have nan=float('nan') but for practical
-        purposes we'll normally report 0.0.
+        purposes we'll normally report None (or 0.0?).
     """
     try:
         return ( float(n) * mul ) / float(d)
